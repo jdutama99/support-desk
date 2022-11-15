16d225ca19024fdc24b0112c2d452bd5d989f049
Jason <jasonutama0206@gmail.com>
2022-11-15 18:08:15 -0500

    login and register functionality

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
+
+function Home() {
+  return (
+    <>
+      <section className="heading">
+        <h1>Do you need assistance?</h1>
+        <p> Please choose from option below</p>
+      </section>
+      <Link to="/new-ticket" className="btn btn-reverse btn-block">
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+      <Link to="/new-ticket" className="btn btn-block">
+        <FaTicketAlt /> View My Ticket
+      </Link>
+    </>
+  )
+}
+
+export default Home
